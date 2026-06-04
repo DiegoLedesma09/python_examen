4df7a1df71bdf76d68f0934b9b88cfa8883af51b
Diego Ledesma Gallegos <diegolg@lcg.unam.mx>
2026-06-03 19:43:33 -0600

    function: deseq funcional, documentacion añadida, resultados preliminares

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-﻿import pandas as pd
-
-def main():
-    print("¡Proyecto de examen final iniciado!")
+﻿import os
+import sys
 
 if __name__ == "__main__":
+    package_dir = os.path.join(os.path.dirname(__file__), "src")
+    if package_dir not in sys.path:
+        sys.path.insert(0, package_dir)
+
+    from deseq_utils import main
     main()
